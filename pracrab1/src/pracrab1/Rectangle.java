52ccd570911a08b5428c7d8324bf86ff6be548fa
IgorKalugin <igori.kalugin@mail.ru>
2020-09-13 17:12:22 +0300

    version 1.0

---
+++ pracrab1/src/pracrab1/Rectangle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pracrab1;
+
+/**
+ *
+ * @author ded
+ */
+public class Rectangle {
+    public int rectNum;
+    public double lX;
+    public double lY;
+    public double rX;
+    public double rY;
+    public Rectangle (int rN, double lX, double lY, double rX, double rY) {
+        this.rectNum = rN;
+        this.lX = lX;
+        this.lY = lY;
+        this.rX = rX;
+        this.rY = rY;
+    }
+}
